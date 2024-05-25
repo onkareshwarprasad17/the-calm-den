18753167f8099c630146e6175bca4f5500ca182c
Onkareshwar Prasad <onkareshwarkupra@gmail.com>
2024-05-25 16:05:10 +0530

    feature(modal): updated modal code with compound component pattern

--- src/features/cabins/AddCabin.jsx
+++ src/features/cabins/AddCabin.jsx
@@ -1,20 +1,37 @@
-import { useState } from "react";
 import Button from "../../ui/Button";
 import Modal from "../../ui/Modal";
 import CreateCabinForm from "./CreateCabinForm";
 
 function AddCabin() {
-  const [isModalOpen, setIsModalOpen] = useState(false);
+  /*
+        Issue with previous implementation (see OldModal.jsx):-
+            1. The component that needs Modal have to maintain a state to check whether the modal is open or not
+            2. Due to the above fact, the component would also need to pass the state update function as well to the children or button as needed.
+        
+        Why this is a issue?
+            As per the old implementation, it would be the responsibility of the component implementing the modal maintain the state logic of modal opening and closing.
+            But in fact, THIS should be the responsibility of the MODAL to check if it is open or not, and then render the children as passed.
+
+    */
   return (
     <>
-      <Button onClick={() => setIsModalOpen((show) => !show)}>
-        Add New Cabin
-      </Button>
-      {isModalOpen && (
-        <Modal onClose={() => setIsModalOpen(false)}>
-          <CreateCabinForm onCloseModal={() => setIsModalOpen(false)} />
-        </Modal>
-      )}
+      <Modal>
+        <Modal.Open opens="cabin-form">
+          <Button>Add New Cabin</Button>
+        </Modal.Open>
+        <Modal.Window name="cabin-form">
+          <CreateCabinForm />
+        </Modal.Window>
+
+        {/* Multiple Window example */}
+
+        {/* <Modal.Open opens="table">
+          <Button>Show Table</Button>
+        </Modal.Open>
+        <Modal.Window name="table">
+          <CabinTable />
+        </Modal.Window> */}
+      </Modal>
     </>
   );
 }
