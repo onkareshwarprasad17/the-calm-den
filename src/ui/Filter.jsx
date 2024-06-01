f90f5ec8a98a9c79d757c8adc24504f6fd1b9176
Onkareshwar Prasad <onkareshwarkupra@gmail.com>
2024-06-01 20:15:58 +0530

    feat(filter): implemented filter functionality for cabins

---
+++ src/ui/Filter.jsx
@@ -0,0 +1,71 @@
+import { useSearchParams } from "react-router-dom";
+import styled, { css } from "styled-components";
+
+const StyledFilter = styled.div`
+  border: 1px solid var(--color-grey-100);
+  background-color: var(--color-grey-0);
+  box-shadow: var(--shadow-sm);
+  border-radius: var(--border-radius-sm);
+  padding: 0.4rem;
+  display: flex;
+  gap: 0.4rem;
+`;
+
+const FilterButton = styled.button`
+  background-color: var(--color-grey-0);
+  border: none;
+
+  ${(props) =>
+    props.active === "true" &&
+    css`
+      background-color: var(--color-brand-600);
+      color: var(--color-brand-50);
+    `}
+
+  border-radius: var(--border-radius-sm);
+  font-weight: 500;
+  font-size: 1.4rem;
+  /* To give the same height as select */
+  padding: 0.44rem 0.8rem;
+  transition: all 0.3s;
+
+  &:hover:not(:disabled) {
+    background-color: var(--color-brand-600);
+    color: var(--color-brand-50);
+  }
+`;
+
+function Filter() {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const filterValue = searchParams.get("discount");
+
+  function handleClick(filterOption) {
+    searchParams.set("discount", filterOption);
+    setSearchParams(searchParams);
+  }
+
+  return (
+    <StyledFilter>
+      <FilterButton
+        onClick={() => handleClick("all")}
+        active={(filterValue === "all").toString()} // to avoid non-boolean issue on the attribute
+      >
+        All
+      </FilterButton>
+      <FilterButton
+        onClick={() => handleClick("no-discount")}
+        active={(filterValue === "no-discount").toString()}
+      >
+        No Discount
+      </FilterButton>
+      <FilterButton
+        onClick={() => handleClick("with-discount")}
+        active={(filterValue === "with-discount").toString()}
+      >
+        With Discount
+      </FilterButton>
+    </StyledFilter>
+  );
+}
+
+export default Filter;
