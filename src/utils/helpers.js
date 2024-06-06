aa148303b34e73a55edafef98615500626918a5d
Onkareshwar Prasad <onkareshwarkupra@gmail.com>
2024-06-06 21:10:40 +0530

    feat(bookings): added logic to upload sample json to DB

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,4 +1,9 @@
-import { formatDistance, parseISO } from "date-fns";
+import { differenceInDays, formatDistance, parseISO } from "date-fns";
+
+// We want to make this function work for both Date objects and strings (which come from Supabase)
+export const subtractDates = (dateStr1, dateStr2) =>
+  differenceInDays(parseISO(String(dateStr1)), parseISO(String(dateStr2)));
+
 export const formatCurrency = (value) =>
   new Intl.NumberFormat("en", {
     style: "currency",
