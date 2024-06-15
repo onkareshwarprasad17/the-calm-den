a200fb34214335e97d1da82ccdffcdf145476326
Onkareshwar Prasad <onkareshwarkupra@gmail.com>
2024-06-15 16:02:46 +0530

    feat(profile): feature to update user profile data

---
+++ src/features/authentication/useUpdateUser.js
@@ -0,0 +1,17 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { updateCurrentUser } from "../../services/apiAuth";
+import toast from "react-hot-toast";
+
+export function useUpdateUser() {
+  const queryClient = useQueryClient();
+  const { mutate: updateUser, isPending: isUpdating } = useMutation({
+    mutationFn: updateCurrentUser,
+    onSuccess: ({ user }) => {
+      toast.success("User account successfully updated");
+      queryClient.setQueryData(["user"], user);
+    },
+    onError: (error) => toast.error(error.message),
+  });
+
+  return { updateUser, isUpdating };
+}
--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -43,3 +43,35 @@ export async function signup({ fullName, email, password }) {
 
   return data;
 }
+
+export async function updateCurrentUser({ password, fullName, avatar }) {
+  // 1. Update password OR fullName
+  let updateData;
+  if (password) updateData = { password };
+  if (fullName) updateData = { data: { fullName } };
+  const { data, error } = await supabase.auth.updateUser(updateData);
+
+  if (error) throw new Error(error.message);
+  if (!avatar) return data;
+
+  // 2. Upload avatar image
+  const avatarFileName = `avatar-${data.user.id}-${Math.random()}`;
+
+  const { error: storageError } = await supabase.storage
+    .from("avatars")
+    .upload(avatarFileName, avatar);
+
+  if (storageError) throw new Error(storageError.message);
+
+  // 3. update current user
+  const { data: updatedUser, error: updateError } =
+    await supabase.auth.updateUser({
+      data: {
+        avatar: `https://nhpmbvwbprsuwjkodyzy.supabase.co/storage/v1/object/public/avatars/${avatarFileName}`,
+      },
+    });
+
+  if (updateError) throw new Error(updateError.message);
+
+  return updatedUser;
+}
