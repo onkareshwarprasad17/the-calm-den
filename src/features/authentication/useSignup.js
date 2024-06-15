34f39f8801f408639dfd7d0efcc5ce62d0dbaec9
Onkareshwar Prasad <onkareshwarkupra@gmail.com>
2024-06-15 16:00:43 +0530

    feat(users): added logic to register other users into system

---
+++ src/features/authentication/useSignup.js
@@ -0,0 +1,20 @@
+import { useMutation } from "@tanstack/react-query";
+import { signup as signupApi } from "../../services/apiAuth";
+import toast from "react-hot-toast";
+
+export function useSignup() {
+  const { mutate: signup, isPending } = useMutation({
+    mutationFn: signupApi,
+    onSuccess: () => {
+      toast.success(
+        "Account successfully created! Please verufy the new account from the user's email address."
+      );
+    },
+    onError: (err) => {
+      console.error(err.message);
+      toast.error("Error occurred while signing up, error: ", err);
+    },
+  });
+
+  return { signup, isPending };
+}
