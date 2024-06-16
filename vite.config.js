eee6faa22517ef4af6a555a5573dc0b89d6cc0e7
Onkareshwar Prasad <onkareshwarkupra@gmail.com>
2024-06-16 09:44:54 +0530

    chore: added config files

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
+import dotenv from "dotenv";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import eslint from "vite-plugin-eslint";
 
+dotenv.config();
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), eslint()],
+  define: {
+    "process.env": process.env,
+  },
 });
