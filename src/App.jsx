3964096591434a050fe0c522974fbb269a554d0f
Onkareshwar Prasad <onkareshwarkupra@gmail.com>
2024-06-18 00:25:09 +0530

    chore: change logo and commented dev tools

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+// import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Toaster } from "react-hot-toast";
 
 import Dashboard from "./pages/Dashboard";
@@ -30,7 +30,7 @@ function App() {
   return (
     <DarkModeProvider>
       <QueryClientProvider client={queryClient}>
-        <ReactQueryDevtools initialIsOpen={true} />
+        {/* <ReactQueryDevtools initialIsOpen={true} /> */}
 
         <GlobalStyles />
         <BrowserRouter>
